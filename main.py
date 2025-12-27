bc90552e2eda79ff5d0741ef71947b28d599ccf6
Ahmed <AhmedBahlaoui@outlook.com>
2025-12-27 16:02:26 +0100

    added project description

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+## Project de recherche operationnelle - Methode des deux phases
+## Auteurs: BAHLAOUI AHMED, LOGRAINE WIAM, RAMADAN ILIAS, ENNACHKLAOUI AYA
+## Date: 27 Decembre 2025
+## Filiere: INDIA/SD
+## Encadre par: Pr. ES-SADEK
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-
-a = 4
